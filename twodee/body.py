d52ac803a0731cf3206b8b4c74333a8df5eb5867
Ha Jong Kim <yluminance@gmail.com>
2023-08-07 05:00:23 +0200

    quadtree barycenters

--- twodee/body.py
+++ twodee/body.py
@@ -5,8 +5,14 @@ from typing import Self
 
 class Vector2(np.ndarray):
     def __new__(cls, x: float, y: float) -> Self:
-        return np.asarray(np.array([x, y])).view(Vector2)
+        return np.asarray(np.array([x, y], dtype=np.float_)).view(Vector2)
     
+    def __repr__(self) -> str:
+        if self.x == 0 == self.y:
+            return '[0]'
+        else:
+            return f'[{self.x}, {self.y}]'
+
     @property
     def x(self) -> float:
         return self[0]
@@ -14,6 +20,10 @@ class Vector2(np.ndarray):
     @property
     def y(self) -> float:
         return self[1]
+    
+    @classmethod
+    def zero(cls) -> Self:
+        return cls(0, 0)
 
 
 @dataclass
@@ -29,8 +39,17 @@ class Rect2:
         return np.all(self.min <= position) and np.all(position < self.max)
 
 
-@dataclass
+@dataclass(slots=True)
 class Body2:
     mass: float
     pos: Vector2
     vel: Vector2
+
+    @classmethod
+    def zero(cls) -> Self:
+        return cls(0, 0, 0)
+
+    def __add__(self: Self, other: Self) -> Self:
+        return Body2(self.mass + other.mass,
+                     self.pos + other.pos,
+                     self.vel + other.vel)
--- twodee/quadtree.py
+++ twodee/quadtree.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
-from typing import Iterable, Self
+import numpy as np
+from typing import Iterable, Self, Generator
 
 from .body import Body2, Vector2, Rect2
 
 
-@dataclass
+@dataclass(repr=False)
 class Node4:
     boundary: Rect2
 
@@ -12,15 +13,40 @@ class Node4:
     northeast: Self = None
     southwest: Self = None
     southeast: Self = None
-    body: Body2 = None
+    barycenter: Body2 = None
+    _count: int = 0
+
+    def __repr__(self) -> str:
+        return f'Node4({self.count})' if self.count <= 1 else f'Node4({", ".join(str(child.count) for child in self.children)})'
 
     @property
-    def filled(self) -> bool:
-        return self.body is not None
+    def count(self):
+        return self._count
+
+    @property
+    def children(self) -> tuple[Self]:
+        return (self.northwest, self.northeast, self.southwest, self.southeast)
 
     @property
     def subdivided(self) -> bool:
         return self.northwest is not None
+
+    def traverse(self) -> Generator[Self, None, None]:  # post order traversal
+        if self.count == 0:
+            yield self
+            return
+        
+        if self.count != 1:
+            for child in self.children:
+                for node in child.traverse():
+                    yield node
+        
+        yield self
+
+    def calculate_barycenter(self) -> None:
+        self.barycenter = sum((child.barycenter for child in self.children), start=Body2.zero())
+        self.barycenter.pos /= self.count
+        self.barycenter.vel /= self.count
 
     def subdivide(self) -> None:
         boundary_center = self.boundary.center
@@ -41,23 +67,31 @@ class Node4:
         if not self.boundary.contains(body.pos):
             return
 
-        if not self.filled:
-            self.body = body
+        if self.count == 0:
+            self.barycenter = body
+            self._count += 1
             return
         
         if not self.subdivided:
             self.subdivide()
+            self.insert_children(self.barycenter)
+            self.barycenter = None
         
-        self.northwest.insert(body)
-        self.northeast.insert(body)
-        self.southwest.insert(body)
-        self.southeast.insert(body)
+        self.insert_children(body)
+        self._count += 1
+    
+    def insert_children(self, body: Body2) -> None:
+        for child in self.children:
+            child.insert(body)
 
 
 class Quadtree:
     def __init__(self, boundary: Rect2) -> None:
         self.root = Node4(boundary)
     
+    def __repr__(self) -> str:
+        return f'Quadtree({self.root.count})'
+
     @classmethod
     def from_bodies(cls, bodies: Iterable[Body2], contraint: Rect2) -> Self:
         min_x = min(bodies, key=lambda i: i.pos.x).pos.x
@@ -68,4 +102,22 @@ class Quadtree:
         boundary = Rect2(Vector2(min(min_x, contraint.min.x), min(min_y, contraint.min.y)),
                          Vector2(max(max_x, contraint.max.x), max(max_y, contraint.max.y)))
         
-        return cls(boundary)
+        tree = cls(boundary)
+        for body in bodies:
+            tree.insert(body)
+
+        return tree
+
+    def insert(self, body: Body2) -> None:
+        self.root.insert(body)
+
+    def calculate_barycenters(self) -> None:
+        for node in self.root.traverse():
+            if node.count == 0:
+                node.barycenter = Body2(0, Vector2.zero(), Vector2.zero())
+                
+            if node.subdivided:
+                node.calculate_barycenter()
+            
+            
+            
